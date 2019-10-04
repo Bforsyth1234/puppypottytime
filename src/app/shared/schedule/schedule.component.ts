783a2216588b40b8147f1f87469bb55901f0c0c1
Brooks Forsyth <brooks.forsyth@PRFT-13869-MAC.hsd1.ct.comcast.net>
2019-10-04 11:18:17 -0400

    added schedule form

--- src/app/shared/schedule/schedule.component.ts
+++ src/app/shared/schedule/schedule.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-schedule',
@@ -6,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./schedule.component.scss'],
 })
 export class ScheduleComponent implements OnInit {
+  scheduleForm: FormGroup;
 
-  constructor() { }
+  constructor() {
+    this.scheduleForm = new FormGroup({
+      wakeUp: new FormControl('', Validators.required),
+      breakfast: new FormControl('', Validators.required),
+      lunch: new FormControl('', Validators.required),
+      dinner: new FormControl('', Validators.required),
+      bedTime: new FormControl('', Validators.required),
+    });
+  }
 
-  ngOnInit() {}
+  ngOnInit() { }
+
+  onSubmit() {
+    console.log('this.scheduleForm = ');
+    console.log(this.scheduleForm);
+  }
 
 }
--- src/app/tab2/tab2.module.ts
+++ src/app/tab2/tab2.module.ts
@@ -2,7 +2,7 @@ import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Tab2Page } from './tab2.page';
 import { ScheduleComponent } from '../shared/schedule/schedule.component';
 
@@ -11,6 +11,7 @@ import { ScheduleComponent } from '../shared/schedule/schedule.component';
     IonicModule,
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     RouterModule.forChild([{ path: '', component: Tab2Page }])
   ],
   declarations: [Tab2Page, ScheduleComponent]
